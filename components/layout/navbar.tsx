2b2c43798d38c2f1af9504ff15c6da491dbc2de5
Raiiynnn <githubdaffa@gmail.com>
2025-11-23 19:57:48 +0700

    feat:edit navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from 'react';
+import Image from 'next/image'; // Import Image dari Next.js
 import { Menu, X } from 'lucide-react';
 import { navigationLinks } from '@/app/data';
 
@@ -17,9 +18,19 @@ const Navbar = () => {
   return (
     <nav className={`fixed w-full z-50 transition-all duration-500 ${isScrolled ? 'bg-slate-950/80 backdrop-blur-md border-b border-slate-800 py-4 shadow-lg' : 'bg-transparent py-6'}`}>
       <div className="container mx-auto px-6 flex justify-between items-center">
-        <a href="#" className="text-2xl font-bold bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 bg-clip-text text-transparent hover:opacity-80 transition-opacity">
-          DRR.
+        
+        <a href="#" className="hover:opacity-80 transition-opacity relative flex items-center">
+            <div className="relative w-12 h-12 md:w-12 md:h-12">
+                <Image 
+                    src="/D-Logo.png" 
+                    alt="Logo Brand" 
+                    fill
+                    className="object-contain" 
+                    priority
+                />
+            </div>
         </a>
+        {/* ------------------- */}
 
         <div className="hidden md:flex space-x-8">
           {navigationLinks.map((link) => (
@@ -37,12 +48,17 @@ const Navbar = () => {
       
       {isMenuOpen && (
           <div className="md:hidden absolute top-full left-0 w-full bg-slate-900/95 backdrop-blur-lg border-b border-slate-800 p-4 flex flex-col space-y-4 shadow-2xl animate-in slide-in-from-top-5 duration-300">
-          {navigationLinks.map((link) => (
-            <a key={link.name} href={link.href} onClick={() => setIsMenuOpen(false)} className="text-slate-300 hover:text-teal-400 font-medium py-2 border-b border-slate-800/50">
-              {link.name}
-            </a>
-          ))}
-        </div>
+            {navigationLinks.map((link) => (
+              <a 
+                key={link.name} 
+                href={link.href} 
+                className="text-slate-300 hover:text-teal-400 font-medium py-2 border-b border-slate-800/50 last:border-0"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {link.name}
+              </a>
+            ))}
+          </div>
       )}
     </nav>
   );
